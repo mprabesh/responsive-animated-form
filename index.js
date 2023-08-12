ac51b5ba7aca3a6a9222f6323fc4c25409877124
mprabesh <theprabesh723@gmail.com>
2023-08-12 19:44:08 +0545

    feat: env and docker file configured

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
 
+PORT = process.env.PORT ? process.env.PORT : 8080;
+
 app.use(express.static("assets"));
 
-app.listen(3001, () => {
-  console.log("Listening to port 3001");
+app.listen(PORT, () => {
+  console.log("Listening to port", PORT);
 });
